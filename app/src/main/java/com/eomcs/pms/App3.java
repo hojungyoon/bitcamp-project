93f1557db01630d39e2e736842789bedd97eeced
hojungyoon <ssuykkif29@gmail.com>
2021-01-15 00:12:27 +0900

    v3

--- app/src/main/java/com/eomcs/pms/App3.java
+++ app/src/main/java/com/eomcs/pms/App3.java
@@ -1,55 +1,38 @@
 package com.eomcs.pms;
-import java.sql.Date;
+
+import java.util.Date;
 import java.util.Scanner;
 
 public class App3 {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.println("[작업]");
-
-    System.out.print("프로젝트? ");
-    String project = scanner.nextLine();
+    java.io.InputStream keybord = System.in;
+    Scanner keyScan= new Scanner(keybord);
+    Date day = new Date();
 
     System.out.print("번호? ");
-    int number = scanner.nextInt();
+    String number = keyScan.nextLine();
 
-    scanner.nextLine();
+    System.out.print("이름? ");
+    String name = keyScan.nextLine();
 
-    System.out.print("내용? ");
-    String contents = scanner.nextLine();
+    System.out.print("이메일? ");
+    String eMail = keyScan.nextLine();
 
-    System.out.print("완료일? ");
-    Date endDay = Date.valueOf(scanner.nextLine());
+    System.out.print("암호? ");
+    String passWord = keyScan.nextLine();
 
-    System.out.print("상태?\n");
-    System.out.print("0: 신규\n");
-    System.out.print("1: 진행중\n");
-    System.out.print("2: 완료\n");
-    System.out.print("> ");
-    int choice = scanner.nextInt();
+    System.out.print("사진? ");
+    String picture = keyScan.nextLine();
 
-    scanner.nextLine();
+    System.out.print("전화? ");
+    String callNumber = keyScan.nextLine();
 
-    System.out.print("담장자? \n");
-    String owner = scanner.nextLine();
 
-    System.out.println("-----------------------------");
+    System.out.println("----------------------------");
+    System.out.printf("번호: %s\n이름: %s\n이메일: %s\n암호: %s\n사진: %s\n전화: %s\n",number, name, eMail, passWord, picture, callNumber);
+    System.out.printf("가입일: %1$tY-%1$tM-%1$td", day);
 
-    System.out.printf("프로젝트: %s\n", project);
-    System.out.printf("번호: %s\n", number);
-    System.out.printf("내용: %s\n", contents);
-    System.out.printf("완료: %s\n", endDay);
-
-    if(choice == 0) {
-      System.out.println("상태: 신규");
-    }else if(choice == 1) {
-      System.out.println("상태: 진행중");
-    }else if(choice == 2) {
-      System.out.println("상태: 완료");
-    }
-
-    System.out.printf("담당자: %s\n", owner);
+    keyScan.close();
 
 
   }
---
+++ app/src/main/java/com/eomcs/pms/App4.java
@@ -0,0 +1,11 @@
+package com.eomcs.pms;
+
+public class App4 {
+  public static void main(String[] args) {
+    int a = 10;
+    short b = 20;
+
+    System.out.println(a+b);
+
+  }
+}
