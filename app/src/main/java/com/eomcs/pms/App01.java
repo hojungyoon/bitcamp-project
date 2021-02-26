f6bf14e7b8732bb7c632c5ecea1273bef7d35a04
hojungyoon <ssuykkif29@gmail.com>
2021-02-26 17:26:56 +0900

    .

--- app/src/main/java/com/eomcs/pms/App01.java
+++ app/src/main/java/com/eomcs/pms/App01.java
@@ -1,19 +1,13 @@
 package com.eomcs.pms;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.sql.Date;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 import com.eomcs.pms.domain.Board;
 import com.eomcs.pms.domain.Member;
 import com.eomcs.pms.domain.Project;
@@ -44,32 +38,72 @@ import com.eomcs.pms.handler.TaskListHandler;
 import com.eomcs.pms.handler.TaskUpdateHandler;
 import com.eomcs.util.Prompt;
 
-public class App {
+// 1) 게시글 데이터 로딩 및 저장 (메서드로 분리하기 전)
+public class App01 {
 
   // 사용자가 입력한 명령을 저장할 컬렉션 객체 준비
   static ArrayDeque<String> commandStack = new ArrayDeque<>();
   static LinkedList<String> commandQueue = new LinkedList<>();
 
-  // VO 를 저장할 컬렉션 객체
-  static List<Board> boardList;
-  static List<Member> memberList;
-  static List<Project> projectList;
-  static List<Task> taskList;
-
-  // 데이터 파일 정보
-  static File boardFile = new File("boards.data");
-  static File memberFile = new File("members.data");
-  static File projectFile = new File("projects.data");
-  static File taskFile = new File("tasks.data");
 
   public static void main(String[] args) {
 
+    ArrayList<Board> boardList = new ArrayList<>();
+    ArrayList<Member> memberList = new ArrayList<>();
+    LinkedList<Project> projectList = new LinkedList<>();
+    LinkedList<Task> taskList = new LinkedList<>();
 
     // 파일에서 데이터를 읽어온다.(데이터 로딩)
-    boardList = loadObjects(boardFile, Board.class);
-    memberList = loadObjects(memberFile, Member.class);
-    projectList = loadObjects(projectFile, Project.class);
-    taskList = loadObjects(taskFile, Task.class);
+    try (FileInputStream in = new FileInputStream("boards.data")) {
+      // boards.data 파일 포맷에 따라 데이터를 읽는다.
+      // 1) 게시글 개수
+      int size = in.read() << 8 | in.read();
+
+      // 2) 게시글 개수 만큼 게시글을 읽는다.
+      for (int i = 0; i < size; i++) {
+        // 게시글 데이터를 저장할 객체 준비
+        Board b = new Board();
+
+        // 게시글 데이터를 읽어서 객체에 저장
+        // - 게시글 번호를 읽어서 객체에 저장
+        b.setNo(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
+
+        // - 게시글 제목을 읽어서 객체에 저장
+        int len = in.read() << 8 | in.read();
+        byte[] buf = new byte[len];
+        in.read(buf);
+        b.setTitle(new String(buf, "UTF-8"));
+
+        // - 게시글 내용을 읽어서 객체에 저장
+        len = in.read() << 8 | in.read();
+        buf = new byte[len];
+        in.read(buf);
+        b.setContent(new String(buf, "UTF-8"));
+
+        // - 게시글 작성자 읽어서 객체에 저장
+        len = in.read() << 8 | in.read();
+        buf = new byte[len];
+        in.read(buf);
+        b.setWriter(new String(buf, "UTF-8"));
+
+        // - 게시글 등록일을 읽어서 객체에 저장
+        len = in.read() << 8 | in.read();
+        buf = new byte[len];
+        in.read(buf);
+        b.setRegisteredDate(Date.valueOf(new String(buf, "UTF-8")));
+
+        // - 게시글 조회수를 읽어서 객체에 저장
+        b.setViewCount(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
+
+        // 게시글 객체를 컬렉션에 저장
+        boardList.add(b);
+      }
+      System.out.println("게시글 데이터 로딩!");
+
+    } catch (Exception e) {
+      System.out.println("게시글 데이터 로딩 중 오류 발생!");
+    }
+
 
     // 사용자 명령을 처리하는 객체를 맵에 보관한다.
     HashMap<String,Command> commandMap = new HashMap<>();
@@ -147,10 +181,74 @@ public class App {
       }
 
     // 게시글 데이터를 파일로 출력한다.
-    saveObjects(boardFile, boardList);
-    saveObjects(memberFile, memberList);
-    saveObjects(projectFile, projectList);
-    saveObjects(taskFile, taskList);
+
+    try (FileOutputStream out = new FileOutputStream("boards.data")) {
+
+      // boards.data 파일 포맷
+      // - 2바이트: 저장된 게시글 개수
+      // - 게시글 데이터 목록
+      //   - 4바이트: 게시글 번호
+      //   - 게시글 제목
+      //     - 2바이트: 게시글 제목의 바이트 배열 개수
+      //     - x바이트: 게시글 제목의 바이트 배열
+      //   - 게시글 내용
+      //     - 2바이트: 게시글 내용의 바이트 배열 개수
+      //     - x바이트: 게시글 내용의 바이트 배열
+      //   - 작성자
+      //     - 2바이트: 작성자의 바이트 배열 개수
+      //     - x바이트: 작성자의 바이트 배열
+      //   - 등록일
+      //     - 2바이트: 등록일의 바이트 배열 개수
+      //     - x바이트: 등록일의 바이트 배열
+      //   - 4바이트: 조회수
+      int size = boardList.size();
+      out.write(size >> 8);
+      out.write(size);
+
+      for (Board b : boardList) {
+        // 게시글 번호
+        out.write(b.getNo() >> 24);
+        out.write(b.getNo() >> 16);
+        out.write(b.getNo() >> 8);
+        out.write(b.getNo());
+
+        // 게시글 제목
+        byte[] buf = b.getTitle().getBytes("UTF-8");
+        // - 게시글 제목의 바이트 개수
+        out.write(buf.length >> 8);
+        out.write(buf.length);
+        // - 게시글 제목의 바이트 배열
+        out.write(buf);
+
+        // 게시글 내용
+        buf = b.getContent().getBytes("UTF-8");
+        out.write(buf.length >> 8);
+        out.write(buf.length);
+        out.write(buf);
+
+        // 작성자
+        buf = b.getWriter().getBytes("UTF-8");
+        out.write(buf.length >> 8);
+        out.write(buf.length);
+        out.write(buf);
+
+        // 등록일
+        buf = b.getRegisteredDate().toString().getBytes("UTF-8");
+        out.write(buf.length >> 8);
+        out.write(buf.length);
+        out.write(buf);
+
+        // 조회수
+        out.write(b.getViewCount() >> 24);
+        out.write(b.getViewCount() >> 16);
+        out.write(b.getViewCount() >> 8);
+        out.write(b.getViewCount());
+      }
+      System.out.println("게시글 데이터 저장!");
+
+    } catch (Exception e) {
+      System.out.println("게시글 데이터를 파일로 저장하는 중에 오류 발생!");
+    }
 
     Prompt.close();
   }
@@ -167,33 +265,4 @@ public class App {
       }
     }
   }
-
-  @SuppressWarnings("unchecked")
-  static <T extends Serializable> List<T> loadObjects(File file, Class<T> dataType) {
-    try (ObjectInputStream in = new ObjectInputStream(
-        new BufferedInputStream(new FileInputStream(file)))) {
-
-      System.out.printf("파일 %s 로딩!", file.getName());
-      return (List<T>) in.readObject();
-
-    } catch (Exception e) {
-      System.out.printf("파일 %s 로딩 중 오류 발생!", file.getName());
-      return new ArrayList<T>();
-    }
-  }
-
-  static <T extends Serializable> void saveObjects(File file, List<T> dataList) {
-    try (ObjectOutputStream out = new ObjectOutputStream(
-        new BufferedOutputStream(
-            new FileOutputStream(file)))) {
-
-      out.writeObject(dataList);
-      System.out.printf("파일 %s 저장!\n", file.getName());
-
-    } catch (Exception e) {
-      System.out.printf("파일 %s 저장 중 오류 발생!", file.getName());
-    }
-  }
-
-
 }
